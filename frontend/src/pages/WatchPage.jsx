f14d8e94bc420086608c3eefb5504ca23b4c9e4c
monkeydub29 <133358170+monkeydub29@users.noreply.github.com>
2025-04-24 09:41:32 +0530

    Update WatchPage.jsx

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useContentStore } from "../store/content";
 import axios from "axios";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/NavBar";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import ReactPlayer from "react-player";
 import { ORIGINAL_IMG_BASE_URL, SMALL_IMG_BASE_URL } from "../utils/constant";
