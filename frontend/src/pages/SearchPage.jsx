7a017e950237cf8d571298f37cc85e02543ed276
monkeydub29 <133358170+monkeydub29@users.noreply.github.com>
2025-04-24 09:38:25 +0530

    Update SearchPage.jsx

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useContentStore } from "../store/content";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/NavBar";
 import { Search } from "lucide-react";
 import toast from "react-hot-toast";
 import axios from "axios";
